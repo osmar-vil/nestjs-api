c18e53a3e91982d235b931b879fee40e3ec81d90
Osmar L. Villaba <osmarvillalbagonzalez@gmail.com>
2024-02-01 07:54:26 -0300

    FEAT: Validated order create request - added class-transform and class-validator libraries. - included validator pipe to next bootstrap. - mapped order create DTO validations.

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,9 +1,34 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
+
 export class CreateOrderDto {
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => CreateOrderItemDto)
   items: CreateOrderItemDto[];
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   card_hash: string;
 }
 
 export class CreateOrderItemDto {
+  @IsPositive()
+  @IsInt()
+  @IsNotEmpty()
   quantity: number;
+
+  @MaxLength(36)
+  @IsString()
+  @IsNotEmpty()
   product_id: string;
 }
