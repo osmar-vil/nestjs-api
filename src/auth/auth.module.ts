1a2525679c34c81e77ed568fd4f3b277e8897103
Osmar L. Villaba <osmarvillalbagonzalez@gmail.com>
2024-02-02 11:04:44 -0300

    FEAT: add auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { Client } from 'src/clients/entities/client.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthGuard } from './auth.guard';
 
+@Global()
 @Module({
   imports: [
     JwtModule.register({
+      global: true,
       secret: '123456789', // TODO: add it .env
       signOptions: { expiresIn: '60s' },
     }),
     TypeOrmModule.forFeature([Client]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
+  exports: [AuthGuard],
 })
 export class AuthModule {}
